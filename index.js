5c9c9c14d9bca9776e54f96251bb0dd007859800
Ian Groff <ian@iangroff.com>
2015-09-21 14:33:17 -0500

    making this stupid simple

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-var app = require('express')();
-var _   = require('underscore');
-var server = require('http').createServer(app);
-var WebSocketServer = require('ws').Server;
+var http = require('http');
 
-app.get('/.test', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
-});
-app.get('/*', function(request, response) {
-  ret_object = {};
-  _.extend(ret_object, request.headers);
-  _.extend(ret_object, request.params);
-  response.send(ret_object);
-});
 var port = process.env.PORT || 3000;
 
-
-server.listen(port);
-var wsserver = new WebSocketServer({server: server});
-wsserver.on('connection', function(socket){
-  socket.on('message', function(data){
-    console.log(data);
-    socket.send(data);
+var requestHandler = function(request, response){
+  ret_object = {
+    "headers": request.headers,
+    "url": request.url,
+    "method": request.method,
+    "httpVersion": request.httpVersion,
+  };
+  var body = '';
+  request.on('data',  function(data){
+    body += data.toString();
   });
-});
-
-
+  request.on('end', function(){
+    ret_object.body = body;
+    var response_string = JSON.stringify(ret_object);
+    response.writeHead(200, {
+      'Content-Length': response_string.length,
+      'Content-Type': 'application/json'
+      });
+    response.write(response_string);
+    response.end();
+  });
+}
+http.createServer(requestHandler).listen(port);
 console.log('Listening on ' + port);
