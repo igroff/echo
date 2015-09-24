e10f714628d8c47bf26dea1311a34f2c39a01302
Ian Groff <ian@iangroff.com>
2015-09-24 18:58:28 -0500

    just cleaning things up

--- echo.py
+++ echo.py
@@ -1,10 +1,9 @@
 #! /usr/bin/env python
+from email.message import Message
 import BaseHTTPServer
 import logging
 import json
-from email.message import Message
 import os
-
 
 BaseHTTPServer.MessageClass = Message
 
@@ -14,22 +13,17 @@ class EchoHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         method=self.command,
         uri=self.path,
         httpVersion=self.request_version,
-        requestHeaders={}
+        requestHeaders={key:self.headers[key] for key in self.headers.keys()}
     )
-    for key in self.headers.keys():
-        resp_data['requestHeaders'][key] = self.headers[key]
-    
     self.send_response(200, '')
     self.send_header('Content-Type', 'application/json')
     self.end_headers()
     self.wfile.write(json.dumps(resp_data))
 
   def do_GET(self):
-    logging.info("get")
     self.handle_request()
     
   def do_POST(self):
-    logging.info("post")
     self.handle_request()
 
 server = BaseHTTPServer.HTTPServer(('', int(os.environ.get('PORT', 8080))), EchoHandler)
