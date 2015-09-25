937d39b681788aa46606b0845a55f65bde13e6b1
Ian Groff <ian@iangroff.com>
2015-09-24 19:28:25 -0500

    adding body handling

--- echo.py
+++ echo.py
@@ -8,23 +8,26 @@ import os
 BaseHTTPServer.MessageClass = Message
 
 class EchoHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-  def handle_request(self):
-    resp_data = dict(clientAddress=self.client_address[0],
-        method=self.command,
-        uri=self.path,
-        httpVersion=self.request_version,
-        requestHeaders={key:self.headers[key] for key in self.headers.keys()}
-    )
-    self.send_response(200, '')
-    self.send_header('Content-Type', 'application/json')
-    self.end_headers()
-    self.wfile.write(json.dumps(resp_data))
+    # a method for handling our any requests given, simply echo's back
+    # the content of the request including any headers as a json object
+    def handle_request(self):
+        resp_data = dict(clientAddress=self.client_address[0],
+            method=self.command,
+            uri=self.path,
+            httpVersion=self.request_version,
+            requestHeaders={key:self.headers[key] for key in self.headers.keys()}
+        )
+        self.send_response(200, '')
+        self.send_header('Content-Type', 'application/json')
+        request_length = int(resp_data['requestHeaders'].get('content-length', 0))
+        if request_length:
+            resp_data['body'] = self.rfile.read(request_length)
+        self.wfile.write("\n\n%s" % json.dumps(resp_data))
 
-  def do_GET(self):
-    self.handle_request()
+    def __getattr__(self, name):
+        if "do_" in name:
+            return self.handle_request
     
-  def do_POST(self):
-    self.handle_request()
 
 server = BaseHTTPServer.HTTPServer(('', int(os.environ.get('PORT', 8080))), EchoHandler)
 server.serve_forever()
